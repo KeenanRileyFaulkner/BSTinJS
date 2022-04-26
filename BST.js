06e2249e42b46cc9ee13f2597862a3de637e18ec
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-04-26 13:04:18 -0600

    Fixed add functionality... may still have problems with clear

--- BST.js
+++ BST.js
@@ -17,7 +17,7 @@ class BST {
     //clear is a void recursive method and _clear is the helped function (invoke clear not _clear when using tree)
     clear() {
         if(this.root !== null) {
-            _clear(this.root);
+            this._clear(this.root);
         }
     }
 
@@ -26,36 +26,47 @@ class BST {
             return;
         }
 
-        _clear(node.leftChild);
-        _clear(node.rightChild);
+        this._clear(node.leftChild);
+        this._clear(node.rightChild);
 
+        console.log("Clearing", node);
         node = null;
     }
 
     //add returns bool if added (will not add duplicates)
     _add(node, toAdd) {
-        if(node === null) {
-            //this is the place
-            node = toAdd;
-            console.log("Added", toAdd);
-            return true;
-        }
 
         if(node.value === toAdd.value) { //value is already in tree
             console.log("The value is already in the tree");
             return false;
         }else if(node.value > toAdd.value) { //value is less than current node
-            console.log("Went left");
-            return _add(node.leftChild, toAdd);
+            if(node.leftChild === null) {
+                node.leftChild = toAdd;
+                console.log("Added", toAdd);
+                return true;
+            } else {
+                return this._add(node.leftChild, toAdd);
+            }
         } else if (node.value < toAdd.value) { //value is greater than current node
-            console.log("Went right");
-            return _add(node.rightChild, toAdd);
+            if(node.rightChild === null) {
+                console.log("Added", toAdd);
+                node.rightChild = toAdd;
+                return true;
+            } else {
+                return this._add(node.rightChild, toAdd);
+            }
         }
     }
 
     add(value) {
         let nodeToAdd = new Node(value);
-        return this._add(this.root, nodeToAdd);
+        if(this.root === null) {
+            this.root = nodeToAdd;
+            console.log("Added value", value, "to root");
+            return true;
+        } else {
+            return this._add(this.root, nodeToAdd);
+        }
     }
 }
 
@@ -64,3 +75,4 @@ tree.clear();
 tree.add(5);
 tree.add(7);
 tree.add(5);
+tree.clear();
