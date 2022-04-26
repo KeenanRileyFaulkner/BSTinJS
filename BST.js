a1c86cb8d5d93a4c5ff7459a4b98cbf175efd23e
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-04-26 12:49:09 -0600

    Issue: will add nodes but root stays null (no compiler err though)

--- BST.js
+++ BST.js
@@ -15,6 +15,12 @@ class BST {
     }
 
     //clear is a void recursive method and _clear is the helped function (invoke clear not _clear when using tree)
+    clear() {
+        if(this.root !== null) {
+            _clear(this.root);
+        }
+    }
+
     _clear(node) {
         if(node === null) {
             return;
@@ -26,25 +32,35 @@ class BST {
         node = null;
     }
 
-    clear() {
-        if(this.root !== null) {
-            _clear(this.root);
-        }
-    }
-
     //add returns bool if added (will not add duplicates)
     _add(node, toAdd) {
         if(node === null) {
             //this is the place
-            this.root = toAdd;
+            node = toAdd;
+            console.log("Added", toAdd);
             return true;
         }
 
-
+        if(node.value === toAdd.value) { //value is already in tree
+            console.log("The value is already in the tree");
+            return false;
+        }else if(node.value > toAdd.value) { //value is less than current node
+            console.log("Went left");
+            return _add(node.leftChild, toAdd);
+        } else if (node.value < toAdd.value) { //value is greater than current node
+            console.log("Went right");
+            return _add(node.rightChild, toAdd);
+        }
     }
 
     add(value) {
-        nodeToAdd = new Node(value);
-        return _add(this.root, nodeToAdd);
+        let nodeToAdd = new Node(value);
+        return this._add(this.root, nodeToAdd);
     }
 }
+
+let tree = new BST();
+tree.clear();
+tree.add(5);
+tree.add(7);
+tree.add(5);
